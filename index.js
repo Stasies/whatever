d6d9b1ed6eb3458c0de7df369b1e4be04106fde0
Anastasia <80560435+Stasies@users.noreply.github.com>
2021-09-02 13:22:08 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,32 +2,26 @@ const [{ Server: h1 }, x] = [require('http'), require('express')];
 
 const Router = x.Router();
 const PORT = 4321;
-// const { log } = console;
-// const hu = { 'Content-Type': 'text/html; charset=utf-8' };
+const { log } = console;
+const hu = { 'Content-Type': 'text/html; charset=utf-8' };
 const app = x();
-// const mw0 = (r, rs, n) => rs.status(200).set(hu) && n();
-const CORS = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Methods': 'GET,PUT,DELETE',
-  'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers'
-  };
+const mw0 = (r, rs, n) => rs.status(200).set(hu) && n();
 Router
   .route('/')
-  .get(r => r.res.end('tia.nntr.wth'));
+  .get(r => r.res.end('Привет мир!'));
 app
-.get('/login/', (req, res) => res
-.status(200)
-.set({"Content-Type":"text/html; charset=utf-8", ...CORS})
-.end('tia.nntr.wth')
-)
-.get('/sample/', (req, res) => res
-.status(200)
-.set({"Content-Type":"text/plain; charset=utf-8", ...CORS})
-.end(`function task(x){
-  this.x = x*x;
-  return x*this.x;
-  };`)
-
+  .use(mw0)
+  .use(function workingSetter(req, res, next) {req.working = 'Работает, ура!'; next();})
+  .use(x.static('.'))
+  .use('/', Router)
+  .get('/first', (req, res, next) => {
+    req.app._router.stack.forEach(mw => console.log(mw.name))
+    if (req.query.error == 'yes') return next();   
+    res.send(req.working);
+  })
+  .use((req, res, next) => { req.errorMessage = 'Всё ещё нет'; next(); })
+  .use(r => r.res.status(404).set(hu).send(r.errorMessage))
+  .use((e, r, rs, n) => rs.status(500).set(hu).send(`Ошибка: ${e}`))
   /* .set('view engine', 'pug') */
   .set('x-powered-by', false);
 module.exports = h1(app)
