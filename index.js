ccbe515c3d9a7fd32c526e0d854c2c3faa2e8ee7
Anastasia <80560435+Stasies@users.noreply.github.com>
2021-09-02 13:17:44 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,34 +2,32 @@ const [{ Server: h1 }, x] = [require('http'), require('express')];
 
 const Router = x.Router();
 const PORT = 4321;
-const { log } = console;
-const hu = { 'Content-Type': 'text/html; charset=utf-8' };
+// const { log } = console;
+// const hu = { 'Content-Type': 'text/html; charset=utf-8' };
 const app = x();
-const mw0 = (r, rs, n) => rs.status(200).set(hu) && n();
+// const mw0 = (r, rs, n) => rs.status(200).set(hu) && n();
+const CORS = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET,PUT,DELETE',
+  'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers'
+  };
 Router
   .route('/')
-  .get(r => r.res.end('Привет мир!'));
+  .get(r => r.res.end('tia.nntr.wth'));
 app
-  .use(mw0)
-  .use(function workingSetter(req, res, next) {req.working = 'Работает, ура!'; next();})
-  .use(x.static('.'))
-  .use('/', Router)
-  .get('/login/', (req, res, next) => {
-    req.app._router.stack.forEach(mw => console.log(mw.name))
-    if (req.query.error == 'yes') return next();   
-    res.send("tia.nntr.wth");
-  })
-.get('/sample/', (req, res, next) => {
-    req.app._router.stack.forEach(mw => console.log(mw.name))
-  res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
-    res.send("function task(x){
-this.x = x*x;
-return x*this.x;
-};");
-  })
-  .use((req, res, next) => { req.errorMessage = 'Всё ещё нет'; next(); })
-  .use(r => r.res.status(404).set(hu).send(r.errorMessage))
-  .use((e, r, rs, n) => rs.status(500).set(hu).send(`Ошибка: ${e}`))
+.get('/login/', (req, res) => res
+.status(200)
+.set({"Content-Type":"text/html; charset=utf-8", ...CORS})
+.end('tia.nntr.wth')
+)
+.get('/sample/', (req, res) => res
+.status(200)
+.set({"Content-Type":"text/plain; charset=utf-8", ...CORS})
+.end(`function task(x){
+  this.x = x*x;
+  return x*this.x;
+  };`)
+
   /* .set('view engine', 'pug') */
   .set('x-powered-by', false);
 module.exports = h1(app)
